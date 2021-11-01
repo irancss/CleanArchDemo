047ff5fc99b2e7ec609397bbb77db659220a1cc1
irancssofficial <irancssofficial@gmail.com>
2021-11-02 00:32:26 +0330

    Commit Add Course Controller With View And Create Table of List Courses

---
+++ CleanArch/CleanArch.MVC/Controllers/CourseController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArch.Application.Interfaces;
+
+namespace CleanArch.MVC.Controllers
+{
+    public class CourseController : Controller
+    {
+        private ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        public IActionResult Index()
+        {
+            var data = _courseService.GetAllCourse();
+            return View(data);
+        }
+    }
+}
